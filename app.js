f3f3b076b35cc5049e89aeaf7647d193c6ace36b
Heda90 <psic.delgado@hotmail.com>
2025-02-06 17:16:53 -0800

    Se realizan algunas correcciones

--- app.js
+++ app.js
@@ -4,11 +4,16 @@
 //Se crea el array donde se guardaran los nombres.
 let listaDeAmigos = [];
 
-function agregarLista(identificador, valor = []){
-    let ul = document.querySelector('#' + identificador);
+function agregarLista(identificador, valor){
+    let ul = document.querySelector(identificador);
     let li = document.createElement('li');
     li.appendChild(document.createTextNode(valor));
     ul.appendChild(li);
+}
+
+//Se crea la funcion para limpiar la lista.
+function limpiarLista(identificador){
+    document.querySelector(identificador).innerHTML = "";
 }
 
 //Se crea la funcion para agregar los nombres a la lista.
@@ -16,11 +21,13 @@ function agregarAmigo(){
     let nombreAmigo = document.querySelector('#amigo');
     listaDeAmigos.push(nombreAmigo.value);
 
+    //Se comprueba que no este vacio el input.
     if(nombreAmigo.value == ""){
         alert("Por favor agregue un nombre.");
     }else{
+        limpiarLista('#listaAmigos');
         for (let amigo in listaDeAmigos){
-            agregarLista('listaAmigos', listaDeAmigos[amigo])
+            agregarLista('#listaAmigos', listaDeAmigos[amigo])
         }
     }
 
